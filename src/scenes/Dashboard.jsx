51311e1800912b2771d1ac078d940c5884d601ba
thientantran <thientantm@gmail.com>
2023-07-07 18:03:31 +0700

    Create Header component to reuse in pages

--- src/scenes/Dashboard.jsx
+++ src/scenes/Dashboard.jsx
@@ -1,5 +1,14 @@
+import { Box } from "@mui/material";
 import React from "react";
 
+import Header from "../components/Header";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
 }
