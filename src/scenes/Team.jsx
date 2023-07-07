991e75e439160fa402ab956195a69ef6b1942eb6
thientantran <thientantm@gmail.com>
2023-07-07 22:12:10 +0700

    Data grid for team

--- src/scenes/Team.jsx
+++ src/scenes/Team.jsx
@@ -1,5 +1,76 @@
+import {
+  AdminPanelSettingsOutlined,
+  LockOpenOutlined,
+  SecurityOutlined,
+} from "@mui/icons-material";
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import React from "react";
 
+import Header from "../components/Header";
+import { mockDataTeam } from "../data/mockData";
+import { tokens } from "../theme";
 export default function Team() {
-  return <div>Team</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "Number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "access",
+      headerName: "Access Level",
+      flex: 1,
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            bgcolor={
+              access === "admin"
+                ? colors.greenAccent[600]
+                : access === "manager"
+                ? colors.greenAccent[700]
+                : colors.greenAccent[700]
+            }
+            border="4px"
+          >
+            {access === "admin" && <AdminPanelSettingsOutlined />}
+            {access === "manager" && <SecurityOutlined />}
+            {access === "user" && <LockOpenOutlined />}
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
+  return (
+    <Box m="20px">
+      <Header title="TEAM" subtitle="Managing the Team members" />
+      <Box>
+        <DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
+      </Box>
+    </Box>
+  );
 }
