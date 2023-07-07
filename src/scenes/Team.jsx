37ee36fab8f504e6cb1a63b65fa240d0f6fa4b24
thientantran <thientantm@gmail.com>
2023-07-07 22:19:31 +0700

    Finished data grid for team page

--- src/scenes/Team.jsx
+++ src/scenes/Team.jsx
@@ -20,6 +20,7 @@ export default function Team() {
       headerName: "Name",
       flex: 1,
       cellClassName: "name-column--cell",
+      //chỗ này để ở những cell này sẽ có 1 cái class name, mình dễ dàng css hơn
     },
     {
       field: "age",
@@ -68,7 +69,35 @@ export default function Team() {
   return (
     <Box m="20px">
       <Header title="TEAM" subtitle="Managing the Team members" />
-      <Box>
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
         <DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
       </Box>
     </Box>
