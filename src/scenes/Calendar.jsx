96f344d1bef3560419fb5a8c455c1dd47f98eec6
thientantran <thientantm@gmail.com>
2023-07-08 16:05:12 +0700

    layout event to calendar

--- src/scenes/Calendar.jsx
+++ src/scenes/Calendar.jsx
@@ -1,18 +1,27 @@
-import { useTheme } from "@mui/material";
+import { formatDate } from "@fullcalendar/core";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import React, { useState } from "react";
 
+import Header from "../components/Header";
 import { tokens } from "../theme";
 
 export default function Calendar() {
   const theme = useTheme();
-  const color = tokens(theme.palette.mode);
+  const colors = tokens(theme.palette.mode);
   const [currentEvent, setCurrentEvent] = useState([]);
 
   const handleDateClick = (selected) => {
     const title = prompt("Please enter a new title for you event");
     const calendarApi = selected.view.calendar;
     calendarApi.unselect();
-
+    //phải 1 khi click lên cái ngày thì phải unselect liền để gõ title vào
     if (title) {
       calendarApi.addEvent({
         id: `${selected.dataStr}-${title}`,
@@ -33,5 +42,46 @@ export default function Calendar() {
       selected.event.remove();
     }
   };
-  return <div>Calendar</div>;
+  return (
+    <Box m="20px">
+      <Header title="Calendar" subtitle="Full Calendar Interactive Page" />
+      <Box display="flex" justifyContent="space-between">
+        <Box
+          flex="1 1 20%"
+          //flex grow shrink width
+          bgcolor={colors.primary[400]}
+          p="15px"
+          borderRadius="4px"
+        >
+          <Typography variant="h5">Events</Typography>
+          <List>
+            {currentEvent.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  bgcolor: colors.greenAccent[500],
+                  margin: "10px 0",
+                  borderRadius: "2px",
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        {/* CALENDAR */}
+      </Box>
+    </Box>
+  );
 }
