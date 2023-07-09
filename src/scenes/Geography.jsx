2e142a88c933fd27ac602763c8d9dbf29210b2ab
thientantran <thientantm@gmail.com>
2023-07-09 13:37:20 +0700

    setup geo chart from the website

--- src/scenes/Geography.jsx
+++ src/scenes/Geography.jsx
@@ -1,5 +1,16 @@
+import { Box } from "@mui/material";
 import React from "react";
 
+import GeoChart from "../components/GeoChart";
+import Header from "../components/Header";
+
 export default function Geography() {
-  return <div>Geography</div>;
+  return (
+    <Box m="20px">
+      <Header title="Bar chart" subtitle="Simple Bar Chart" />
+      <Box height="75vh">
+        <GeoChart />
+      </Box>
+    </Box>
+  );
 }
