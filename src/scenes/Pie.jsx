154e07182f84af9b9823b4e99402f55e7431b045
thientantran <thientantm@gmail.com>
2023-07-09 12:31:02 +0700

    piechart copy and paste copy from the website of nivo

--- src/scenes/Pie.jsx
+++ src/scenes/Pie.jsx
@@ -1,5 +1,16 @@
+import { Box } from "@mui/material";
 import React from "react";
 
+import Header from "../components/Header";
+import PieChart from "../components/PieChart";
+
 export default function Pie() {
-  return <div>Pie</div>;
+  return (
+    <Box m="20px">
+      <Header title="Bar chart" subtitle="Simple Bar Chart" />
+      <Box height="75vh">
+        <PieChart />
+      </Box>
+    </Box>
+  );
 }
