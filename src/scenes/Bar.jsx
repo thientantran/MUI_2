dcaee917e2b5cd9b9d1bc713df059d38155d31ad
thientantran <thientantm@gmail.com>
2023-07-09 10:45:35 +0700

    barchart for novo page

--- src/scenes/Bar.jsx
+++ src/scenes/Bar.jsx
@@ -1,5 +1,16 @@
+import { Box } from "@mui/material";
 import React from "react";
 
+import BarChart from "../components/BarChart";
+import Header from "../components/Header";
+
 export default function Bar() {
-  return <div>Bar</div>;
+  return (
+    <Box m="20px">
+      <Header title="Bar chart" subtitle="Simple Bar Chart" />
+      <Box height="75vh">
+        <BarChart />
+      </Box>
+    </Box>
+  );
 }
