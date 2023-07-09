37e16be420973dc7bc93119668cb97242488c580
thientantran <thientantm@gmail.com>
2023-07-09 13:49:35 +0700

    finished geochart

--- src/components/GeoChart.jsx
+++ src/components/GeoChart.jsx
@@ -1,101 +1,95 @@
+import { useTheme } from "@mui/material";
 import { ResponsiveChoropleth } from "@nivo/geo";
 import React from "react";
-export default function GeoChart() {
+
+import { mockGeographyData as data } from "../data/mockData";
+import { geoFeatures } from "../data/mockGeoFeatures";
+import { tokens } from "../theme";
+// eslint-disable-next-line react/prop-types
+export default function GeoChart({ isDashboard = false }) {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
     <ResponsiveChoropleth
       data={data}
-      features="/* please have a look at the description for usage */"
+      features={geoFeatures.features}
       margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-      colors="nivo"
+      // colors="nivo"
       domain={[0, 1000000]}
       unknownColor="#666666"
       label="properties.name"
       valueFormat=".2s"
-      projectionTranslation={[0.5, 0.5]}
+      projectionScale={isDashboard ? 40 : 150}
+      projectionTranslation={isDashboard ? [0.49, 0.6] : [0.5, 0.5]}
       projectionRotation={[0, 0, 0]}
-      enableGraticule
-      graticuleLineColor="#dddddd"
-      borderWidth={0.5}
+      // enableGraticule
+      // graticuleLineColor="#dddddd"
+      borderWidth={1.5}
       borderColor="#152538"
-      defs={[
-        {
-          id: "dots",
-          type: "patternDots",
-          background: "inherit",
-          color: "#38bcb2",
-          size: 4,
-          padding: 1,
-          stagger: true,
-        },
-        {
-          id: "lines",
-          type: "patternLines",
-          background: "inherit",
-          color: "#eed312",
-          rotation: -45,
-          lineWidth: 6,
-          spacing: 10,
-        },
-        {
-          id: "gradient",
-          type: "linearGradient",
-          colors: [
-            {
-              offset: 0,
-              color: "#000",
-            },
-            {
-              offset: 100,
-              color: "inherit",
-            },
-          ],
-        },
-      ]}
-      fill={[
-        {
-          match: {
-            id: "CAN",
-          },
-          id: "dots",
-        },
-        {
-          match: {
-            id: "CHN",
-          },
-          id: "lines",
-        },
-        {
-          match: {
-            id: "ATA",
-          },
-          id: "gradient",
-        },
-      ]}
-      legends={[
-        {
-          anchor: "bottom-left",
-          direction: "column",
-          justify: true,
-          translateX: 20,
-          translateY: -100,
-          itemsSpacing: 0,
-          itemWidth: 94,
-          itemHeight: 18,
-          itemDirection: "left-to-right",
-          itemTextColor: "#444444",
-          itemOpacity: 0.85,
-          symbolSize: 18,
-          effects: [
-            {
-              on: "hover",
-              style: {
-                itemTextColor: "#000000",
-                itemOpacity: 1,
+      legends={
+        !isDashboard
+          ? [
+              {
+                anchor: "bottom-left",
+                direction: "column",
+                justify: true,
+                translateX: 20,
+                translateY: -100,
+                itemsSpacing: 0,
+                itemWidth: 94,
+                itemHeight: 18,
+                itemDirection: "left-to-right",
+                itemTextColor: colors.grey[100],
+                itemOpacity: 0.85,
+                symbolSize: 18,
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemTextColor: "#ffffff",
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
               },
+            ]
+          : undefined
+      }
+      theme={{
+        // added
+        axis: {
+          domain: {
+            line: {
+              stroke: colors.grey[100],
             },
-          ],
+          },
+          legend: {
+            text: {
+              fill: colors.grey[100],
+            },
+          },
+          ticks: {
+            line: {
+              stroke: colors.grey[100],
+              strokeWidth: 1,
+            },
+            text: {
+              fill: colors.grey[100],
+            },
+          },
         },
-      ]}
+        legends: {
+          text: {
+            fill: colors.grey[100],
+          },
+        },
+        tooltip: {
+          container: {
+            background:
+              theme.palette.mode === "dark" ? colors.grey[700] : undefined,
+          },
+        },
+      }}
     />
   );
 }
--- src/scenes/Geography.jsx
+++ src/scenes/Geography.jsx
@@ -1,14 +1,21 @@
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import React from "react";
 
 import GeoChart from "../components/GeoChart";
 import Header from "../components/Header";
+import { tokens } from "../theme";
 
 export default function Geography() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
     <Box m="20px">
       <Header title="Bar chart" subtitle="Simple Bar Chart" />
-      <Box height="75vh">
+      <Box
+        height="75vh"
+        border={`1px solid ${colors.grey[100]}`}
+        borderRadius="4px"
+      >
         <GeoChart />
       </Box>
     </Box>
