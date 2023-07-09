fa73634d58227422bd01adbaffcd7f89fbd7142f
thientantran <thientantm@gmail.com>
2023-07-09 12:40:35 +0700

    linechart, copy and paste from website, binding

--- src/scenes/Line.jsx
+++ src/scenes/Line.jsx
@@ -1,5 +1,16 @@
+import { Box } from "@mui/material";
 import React from "react";
 
+import Header from "../components/Header";
+import LineChart from "../components/LineChart";
+
 export default function Line() {
-  return <div>Line</div>;
+  return (
+    <Box m="20px">
+      <Header title="Bar chart" subtitle="Simple Bar Chart" />
+      <Box height="75vh">
+        <LineChart />
+      </Box>
+    </Box>
+  );
 }
